d5141d16a2a6bf7b827cc300e2bfb7a1ad930897
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2018-12-14 11:29:49 +0200

    first stab at proxying some knox functionality to the aws client

---
+++ knoxy.js
@@ -0,0 +1,50 @@
+var AWS = require('aws-sdk');
+
+function Knoxy(client) {
+  this.awsClient = client;
+}
+
+Knoxy.prototype.list = function (params, callback) {
+  this.awsClient.listObjects({
+    Prefix: params.prefix
+  }, callback);
+};
+
+Knoxy.prototype.deleteMultiple = function (objects, callback) {
+  this.awsClient.deleteObjects({
+    Delete: {
+      Objects: objects
+    }
+  }, callback);
+};
+
+Knoxy.prototype.putBuffer = function (buffer, filename, headers, callback) {
+  this.awsClient.upload({
+    Body: buffer,
+    Key: filename,
+    CacheControl: headers['Cache-Control'],
+    ContentDisposition: headers['Content-Disposition'],
+    ContentEncoding: headers['Content-Encoding'],
+    ContentLanguage: headers['Content-Language'],
+    ContentLength: headers['Content-Length'],
+    ContentType: headers['Content-Type'],
+    ContentMD5: headers['Content-MD5'],
+    Expires: headers.Expires
+  }, callback);
+};
+
+module.exports = {
+  createClient: function (options) {
+    var opts = options || {};
+    var awsOptions = {
+      accessKeyId: opts.key,
+      secretAccessKey: opts.secret,
+      params: {
+        Bucket: opts.bucket
+      },
+      region: opts.region
+    };
+    var awsClient = new AWS.S3(awsOptions);
+    return new Knoxy(awsClient);
+  }
+};
